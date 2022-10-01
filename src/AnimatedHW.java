97cdada19b006282b9ace600bebb7be34f4e9800
Shreya Prasad <87517376+shreyap05@users.noreply.github.com>
2022-10-01 20:30:56 +0530

    Create AnimatedHW.java

---
+++ src/AnimatedHW.java
@@ -0,0 +1,216 @@
+import java.applet.*;
+import java.awt.*;
+
+public class hello extends Applet {
+  
+  public void init()
+  {
+    resize(500,500);
+    setBackground(Color.white);
+  }
+  
+  public void paint(Graphics g)
+  {
+    g.setColor(Color.red);
+    try{//HELLO
+      //H
+    for(int i=100;i<151;i++)
+    {
+      g.fillOval(50, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=50;i<76;i++)
+    {
+      g.fillOval(i, 125, 5, 5);
+      Thread.sleep(10);
+    }
+    g.setColor(Color.red);
+    for(int i=100;i<151;i++)
+    {
+      g.fillOval(75, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    //E
+    for(int i=100;i<151;i++)
+    {
+      g.fillOval(100, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=100;i<126;i++)
+    {
+      g.fillOval(i, 100, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=100;i<126;i++)
+    {
+      g.fillOval(i, 125, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=100;i<126;i++)
+    {
+      g.fillOval(i, 150, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    //L
+    for(int i=100;i<151;i++)
+    {
+      g.fillOval(150, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    
+    for(int i=150;i<176;i++)
+    {
+      g.fillOval(i, 150, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    //L
+    for(int i=100;i<151;i++)
+    {
+      g.fillOval(200, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=200;i<226;i++)
+    {
+      g.fillOval(i, 150, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    //O
+    for(int i=100;i<151;i++)
+    {
+      g.fillOval(250, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=250;i<276;i++)
+    {
+      g.fillOval(i, 150, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=150;i>99;i--)
+    {
+      g.fillOval(275, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=275;i>249;i--)
+    {
+      g.fillOval(i, 100, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    
+    //WORLD
+    
+      //W
+    for(int i=200;i<251;i++)
+    {
+      g.fillOval(50, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=50;i<63;i++)
+    {
+      g.fillOval(i, 250, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=250;i>226;i--)
+    {
+      g.fillOval(62, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=62;i<76;i++)
+    {
+      g.fillOval(i, 250, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=250;i>199;i--)
+    {
+      g.fillOval(75,i , 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    //O
+    for(int i=200;i<251;i++)
+    {
+      g.fillOval(100, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=100;i<126;i++)
+    {
+      g.fillOval(i, 250, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=250;i>199;i--)
+    {
+      g.fillOval(125, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=125;i>99;i--)
+    {
+      g.fillOval(i, 200, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    //R
+    for(int i=200;i<251;i++)
+    {
+      g.fillOval(150, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=150;i<176;i++)
+    {
+      g.fillOval(i, 200, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    for(int i=200;i<226;i++)
+    {
+      g.fillOval(175, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    for(int i=175;i>149;i--)
+    {
+      g.fillOval(i,225,5,5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    int x=150,y=225;
+    while(x<176)
+    {
+      g.fillOval(x, y, 5, 5);
+      x++;y++;
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    //L
+    for(int i=200;i<251;i++)
+    {
+      g.fillOval(200, i, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.red);
+    
+    for(int i=200;i<226;i++)
+    {
+      g.fillOval(i, 250, 5, 5);
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    
+    //D
+    
+    for(int i=200;i<251;i++)
+    {
+      g.fillOval(250, i, 5, 5);
+      Thread.sleep(10);
+    }
+    x=250;y=200;g.setColor(Color.red);
+    while(x<276)
+    {
+      g.fillOval(x,y,5,5);
+      x++;y++;
+      Thread.sleep(10);
+    }g.setColor(Color.blue);
+    x=275;y=225;
+    while(x>249)
+    {
+      g.fillOval(x, y, 5, 5);
+      x--;y++;
+      Thread.sleep(10);
+    }
+    }
+    catch(Exception e)
+    {}
+      
+  }
+
+}
