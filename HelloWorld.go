fb707529b294f752c3d7822d2dfbbf55c737a2df
Raja Uzair Abdullah <rajauzairabdullah@gmail.com>
2022-10-01 20:22:49 +0500

    Create HelloWorld.go
    Hello World in GoLang

---
+++ HelloWorld.go
@@ -0,0 +1,20 @@
+package main
+  
+// Importing fmt
+import (
+    "fmt"
+)
+  
+// Calling main
+func main() {
+  
+    // Declaring some const variables
+    const name, dept = "Writing Hello World", "HacktoberFest"
+  
+    // Calling print() function
+    fmt.Print(name, " in New Way Thans ", dept)
+  
+    // It is conventional not to worry about any
+    // error returned by Print.
+  
+}
