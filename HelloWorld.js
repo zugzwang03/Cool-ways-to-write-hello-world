4e30179d8c476d59de4dcff1e9a10194b37eae39
chiggshiggs <96922117+chiggshiggs@users.noreply.github.com>
2022-10-01 13:48:31 +0530

    Add files via upload

---
+++ HelloWorld.js
@@ -0,0 +1,16 @@
+//HELLO WORLD IN JAVASCRIPT
+
+//1. Using console.log()
+
+// the hello world program
+console.log('Hello World');
+
+//2. Using alert
+
+// the hello world program
+alert("Hello, World!");
+
+//3. Using document.write()
+
+// the hello world program
+document.write('Hello, World!');
