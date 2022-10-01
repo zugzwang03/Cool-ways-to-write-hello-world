486a290d240762399d8a2e35bdc5fbc1bdb7db26
Shahadat Hossain Hridoy Al <71395891+HridoyHazard@users.noreply.github.com>
2022-10-01 21:47:30 +0600

    #include <bits/stdc++.h>

---
+++ encryption way of Hello World.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+    std::string str = "V_xyz";
+    char v_x;
+    for(auto i:str)
+    {
+        v_x=i;
+        break;
+    }
+    v_x++;
+    std::cout<<"Hello "<<v_x<<"orld";
+    return 0;
+}
